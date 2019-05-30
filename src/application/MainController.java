3d732abedd01833dc111b3bef4ed4e20a473cdc0
Kolade Femi <koladefemi@gmail.com>
2019-05-30 10:56:40 +0100

    initial commit

---
+++ src/application/MainController.java
@@ -0,0 +1,10 @@
+package application;
+
+import javafx.fxml.FXML;
+
+public class MainController {
+
+	String dbUrl = "jdbc:mysql://localhost:3306/bank?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	static String dbuser = "root";
+	static String dbpassword ="pass";
+}
